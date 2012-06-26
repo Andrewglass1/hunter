ba7f593d09e88dea0ac7283d4225236c21849613
Andrewglass1 <Andrewglass1@gmail.com>
2012-06-26 17:24:28 -0400

    styling my face off

--- test/functional/markets_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MarketsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/helpers/merchants_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class MerchantsHelperTest < ActionView::TestCase
-end
